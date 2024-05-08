5e9f4f2bf8a0d1248894bafe588dab5ab70aa290
arminarm002 <alarm.1993@gmail.com>
2024-05-08 13:33:47 +0700

    08/05/2024-13.33

--- pages/submissions.php
+++ pages/submissions.php
@@ -24,7 +24,8 @@ include ($_SERVER['DOCUMENT_ROOT'] . '/spc2024/connectdb.php');
       <div class="col-12">
         <div class="card border-0" style="color: #494949;">
           <h5>Abstract submission open : 1 February - <span class="redmidline">31 March 2024</span>
-            <span class="redmidline" style="color: #f78787;"> 20 April 2024</span><span class="red"> 30 April 2024</span><br>Template for SPC 2024 abstract.
+            <span class="redmidline" style="color: #f78787;"> 20 April 2024</span><span class="red"> 30 April
+              2024</span><br>Template for SPC 2024 abstract.
             <a href="/spc2024/file/download/spc2024-abstract-template.docx">Download File</a><br>Please do not modify
             the format
             and please convert to pdf. for submission.<br><br>Topic :
@@ -51,13 +52,18 @@ include ($_SERVER['DOCUMENT_ROOT'] . '/spc2024/connectdb.php');
               generate qualified new knowledge in Physics and Physics content to be greater than 50%). Plagiarism and
               self-plagiarism are not allowed.</li>
           </ul>
-          <h4><b>** Guidelines on submitting an abstract **</b> >>> <a style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;" href="/spc2024/file/download/guidelines.pdf"
+          <h4><b>** Guidelines on submitting an abstract **</b> >>> <a
+              style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;" href="/spc2024/file/download/guidelines.pdf"
               class="btn btn-warning" target="_blank">Click</a></h4>
-          <h5>Abstracts must be submitted using the MS CMT platform >>> <a style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;" href="https://cmt3.research.microsoft.com/User/Login?ReturnUrl=%2FSPC2024" class="btn btn-warning" target="_blank">Click</a></h5>
+          <h5>Abstracts must be submitted using the MS CMT platform >>> <a
+              style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;"
+              href="https://cmt3.research.microsoft.com/User/Login?ReturnUrl=%2FSPC2024" class="btn btn-warning"
+              target="_blank">Click</a></h5>
           <br>
           <h5>The outcome of the review will be emailed to the main author by 30 April 2024.</h5><br>
           <h5>Note, however, that any accepted abstracts will be cancelled if the presenting author has not registered
-            and paid >>> <a style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;" href="/spc2024/auth/register.php" class="btn btn-warning" target="_blank">Click</a> the
+            and paid >>> <a style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;" href="/spc2024/auth/register.php"
+              class="btn btn-warning" target="_blank">Click</a> the
             registration fee for the
             congress by 15 May 2024.
           </h5>
@@ -70,46 +76,119 @@ include ($_SERVER['DOCUMENT_ROOT'] . '/spc2024/connectdb.php');
     <div class="row" style="margin: 15px auto;">
       <div class="col-12">
         <div class="card border-0" style="color: #494949;">
-          <h5>Journal Publication’s Important details</h5>
-          <p>Each registrant can submit only ONE manuscript for publication consideration.
-            Please select only ONE journal for your full-paper submission.
-            The conference participants who choose to submit a full paper for publication in the
-            <b style="color:red">Conference E-Proceedings</b> will not be able to submit a full paper for publication in
-            the journals.
-          </p>
-          <h5>SPC2024 Publications</h5>
-          <p>
-            1. Journal of Physics: Conference Series (JPCS) – <b style="color:red">Scopus and SJR Indexed</b>
-          </p>
+          <h4>Journal Publication’s Important details</h4>
+          <ul class="unlist">
+            <li>Author whose abstract is accepted and make the presentation in the conference is cordially invited to
+              submit corresponding manuscript to be published in journals as listed</li>
+
+            <ul style="list-style-type: disc;">
+              <li>Current Applied Science and Technology Journal (Scopus & SJR Q4)</li>
+              <li>Thai Journal of Nanoscience and Nanotechnology (TCI-2)</li>
+              <li>Journal of Physics: Conference Series (Scopus & SJR)</li>
+              <li>Official Conference Proceedings</li>
+            </ul>
+
+            <li>Full paper submission is optional.</li>
+            <li>Each registered abstract is subjected to submit <b><u>ONE</u></b> manuscript for publication
+              consideration.</li>
+            <li>The participants who choose to submit full paper in Conference Proceedings will not be able to submit
+              full paper for publication in the journals.</li>
+            <li>Please select only <b><u>ONE</u></b> journal for your full-paper submission depending on author's
+              decision.</li>
+            <li>Please be informed that the scope of the manuscript must align with scope of journal.</li>
+            <li>The participants must submit your full paper for journal publication following guidelines for
+              submission.</li>
+            <li>The submission process and deadline of submission of each journal is varied depending on journal
+              regulation.</li>
+            <li>The submitted manuscript is subjected to peer-review and acceptance of the manuscript is not guaranteed.
+            </li>
+            <li>Upon acceptance, it could take 2-8 months for the accepted manuscript being published depending on
+              publication process of each specific journal.</li>
+            <li>For <u style="color: #b50202;">Conference Proceedings</u>, accepted manuscripts will be published within
+              <u style="color: #b50202;">one month after the conference.</u>
+            </li>
+          </ul>
+          <h4>SPC2024 Publications</h4>
+          <h5>1. Journal of Physics: Conference Series (Scopus & SJR)</h5>
+          <ul class="unlist">
+            <li>To prepare your full paper for submitting to this proceeding, please refer to the Author guidelines and
+              the template can be found in this link >>>
+              <a href="https://publishingsupport.iopscience.iop.org/author-guidelines-for-conference-proceedings"
+                class="btn btn-warning submisspagebutton" target="_blank">Click</a>
+            </li>
+            <li>Please use the provided template >>> <a href="/spc2024/file/download/journal/IOP-ConfSer-template.docx"
+                class="btn btn-warning submisspagebutton" target="_blank">IOP Template</a></li>
+            <li>To submit your manuscript please go to this link
+              <a href="https://www.morressier.com/call-for-papers/65e6eb98a4dbea87c733a6d2"
+                class="btn btn-warning submisspagebutton" target="_blank">Make Submission</a> then you will be
+              asked
+              to provide your email to create your account. The invitation for registration will be sent to your email.
+              After confirmation and registration, you will be able to submit your manuscript.
+            </li>
+            <li>Full manuscript must be submitted directly via the submission system of the proceedings during <b><U>10
+                  May-10 July, 2024.</U></b></li>
+            <li>After acceptance, authors will be asked to pay publication fee of 4,000 baht.</li>
+          </ul>
           <hr>
-          <p>
-            2. Current Applied Science and Technology - <b style="color:red">Scopus (Q3) SJR (Q4) Indexed </b><br>
-            Journal Website : >>>
-            <a style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;" href="https://li01.tci-thaijo.org/index.php/cast" class="btn btn-warning" target="_blank"> Click </a>
-            <br>
-            <br>
-            <img class="border-bottom border-warning w-25" src="/spc2024/img/journals2.jpg" style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
-          </p>
+          <h5>2. Current Applied Science and Technology Journal: CAST (Scopus & SJR Q4)</h5>
+          <ul class="unlist">
+            <li>Paper that is in scope of the CAST are welcome for submitting to this journal.
+              To prepare your full paper for submitting to this journal, please refer to the journal Guide for Authors
+              when preparing manuscripts >>> <a href="https://li01.tci-thaijo.org/index.php/cast/author_guidelines"
+                class="btn btn-warning submisspagebutton" target="_blank">Click</a> and use this provided template
+              <a href="/spc2024/file/download/journal/CAST_Template.docx" class="btn btn-warning submisspagebutton"
+                target="_blank">CAST Template</a>
+            </li>
+            <li>Full paper must be submitted directly to the journal via the submission system of the journal. <a
+                href="https://li01.tci-thaijo.org/index.php/cast/about/submissions"
+                class="btn btn-warning submisspagebutton" target="_blank">Make Submission</a>
+            </li>
+            <li>Please submit the cover letter informing the manuscript is from SPC2024. <a
+                href="/spc2024/file/download/journal/Cover Letter_CAST-SPC2024.docx" class="btn btn-warning submisspagebutton"
+                target="_blank">Download</a></li>
+            <li>Manuscripts can be submitted during <b><u>10 May- 10 July 2024.</u></b></li>
+            <li>There is no publication fee for CAST.</li>
+          </ul>
+          <img class="border-bottom border-warning w-25" src="/spc2024/img/journals2.jpg"
+            style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
           <hr>
-          <p>
-            3. Thai Journal of Nanoscience and Nanotechnology - <b style="color:red">TCI2 Indexed</b><br>
-            Journal Website : >>>
-            <a style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;" href="https://ph05.tci-thaijo.org/index.php/TJNN/index" class="btn btn-warning" target="_blank"> Click
-            </a><br>
-            <br>
-            <img class="border-bottom border-warning w-25" src="/spc2024/img/journals3.jpg" style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
-          </p>
+          <h5>3. Thai Journal of Nanoscience and Nanotechnology (TCI-2)</h5>
+          <ul class="unlist">
+            <li>Papers relating to nanoscience and nanotechnology are welcome for submitting to this journal.</li>
+            <li>To prepare your full paper for submitting to this journal, please refer to the journal Guide for
+              Authors when preparing manuscripts >>> <a
+                href="https://ph05.tci-thaijo.org/index.php/TJNN/guide_for_author"
+                class="btn btn-warning submisspagebutton" target="_blank">Click</a> and use this provided template
+              <a href="/spc2024/file/download/journal/TJNN_Template.docx" class="btn btn-warning submisspagebutton"
+                target="_blank">TJNN Template </a>
+            </li>
+            <li>Full paper must be submitted directly to the journal via the submission system of the journal <a
+                href="https://ph05.tci-thaijo.org/index.php/TJNN/about/submissions"
+                class="btn btn-warning submisspagebutton" target="_blank">Make Submission</a></li>
+            <li>Please submit the cover letter informing the manuscript is from SPC2024. <a
+                href="/spc2024/file/download/journal/Cover Letter_TJNN-SPC2024.docx"
+                class="btn btn-warning submisspagebutton" target="_blank">Download</a></li>
+            <li>Manuscripts can be submitted during <b><u>10 May- 10 July 2024.</u></b></li>
+            <li>There is no publication fee for TJNN.</li>
+          </ul>
+          <img class="border-bottom border-warning w-25" src="/spc2024/img/journals3.jpg"
+            style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
+
           <hr>
           <p>
             4. Conference E-proceedings<br>
             Download Template : >>>
-            <a style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;" href="/spc2024/file/download/spc2024-proceedings.docx" class="btn btn-warning" target="_blank"> Click
+            <a style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;"
+              href="/spc2024/file/download/spc2024-proceedings.docx" class="btn btn-warning" target="_blank"> Click
             </a><br>
             E-Proceedings Submission Guide : >>>
-            <a style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;" href="/spc2024/file/download/E-Proceedings Submission Guide.pdf" class="btn btn-warning" target="_blank"> Click
+            <a style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;"
+              href="/spc2024/file/download/E-Proceedings Submission Guide.pdf" class="btn btn-warning" target="_blank">
+              Click
             </a>
             <br><br>
-            <img class="border-bottom border-warning w-25" src="/spc2024/img/journals4.jpg" style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
+            <img class="border-bottom border-warning w-25" src="/spc2024/img/journals4.jpg"
+              style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
           </p>
         </div>
       </div>
